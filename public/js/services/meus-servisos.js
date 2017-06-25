907f9fd50a24cff8c7528994146eca084d790dca
guilherme <270210.gui@gmail.com>
2017-06-24 23:45:45 -0300

    "comentando meus serviços "

--- public/js/services/meus-servisos.js
+++ public/js/services/meus-servisos.js
@@ -1,6 +1,6 @@
 angular.module('meusServisos', ['ngResource']).factory('recursoFoto',function ($resource) {
 
-    //Nesse arquivo ele vai fazer que eu não instacie toda hora o $resource nos arquivos
+    //Nesse arquivo ele vai fazer que eu não instacie toda hora o $resource nos arquivos fazendo como se fosse uma função
 
     return $resource('/v1/fotos/:fotoId', null, {
         update: {
