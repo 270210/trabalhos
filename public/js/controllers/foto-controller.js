13b872ac3897944e9c1e410c709110a3744182fc
guilherme <270210.gui@gmail.com>
2017-06-25 04:12:35 -0300

    "melhorando a parte do desenvolvedor e comentado"

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,38 +1,29 @@
-angular.module('alurapic').controller('FotoController', function ($scope, recursoFoto,$routeParams) {
+angular.module('alurapic').controller('FotoController', function ($scope, recursoFoto, cadastroDeFoto, $routeParams) {
     $scope.foto = {};
     $scope.mensagem = '';
 
 
-    if($routeParams.fotoId){//caso a url seja para pegar um dado do banco de dados
-        recursoFoto.get({fotoId: $routeParams.fotoId}, function(foto) {
+    if ($routeParams.fotoId) {//caso a url seja para pegar um dado do banco de dados
+        recursoFoto.get({ fotoId: $routeParams.fotoId }, function (foto) {
             $scope.foto = foto;
-        },function(erro) {
+        }, function (erro) {
             console.log(erro);
             $scope.mensagem = "Não foi possivel editar a Foto"
         });
     }
 
     $scope.submeter = function () {//crinado a função submeter para ser usada no foto.html
-        if ($scope.formulario.$valid) {//o formulário só vai entrar aqui se estiver valido
-            if ($scope.foto._id) {//nesse caso se o navegador enteder que for uma altualização ele irá fazer o codigo a seguir
-                recursoFoto.update({fotoId: $scope.foto._id},
-                    $scope.foto,function() {
-                        $scope.mensagem = "A foto" + "" + $scope.foto.titulo + "" + "foi alterada com Sucesso!!!";    
-                    },function(erro) {
-                        console.log(erro);
-                        $scope.mensagem = 'Não foi possivel alterar a foto' + $scope.foto.titulo;           
-                    });
-            
-            } else {//caso o navegador entender que ao salvar for para incluir uma nova foto ele irá fazer o codigo a seguir
-                recursoFoto.save($scope.foto,function() {//nesse bloco ele está colocando a foto no banco de dados
-                    $scope.foto = {};
-                        $scope.mensagem = "Foto Cadastrada com Sucesso!!";
-                },function(erro) {
-                    console.log(erro);//caso aconteça qualquer erro esse codigo íráa ser execultado
-                        $scope.mensagem = "Não foi possivel cadastrar foto";    
-                }); 
-            
-            }
+        if ($scope.formulario.$valid) {//o formulário só vai entrar aqui se estiver valido   
+            cadastroDeFoto.cadastrar($scope.foto)
+                .then(function (dados) {
+                    $scope.mensagem = dados.mensagem;
+                    if (dados.inclusao)
+                        $scope.foto = {};
+
+                })
+                .catch(function (erro) {
+                    $scope.mensagem = erro.mensagem;
+                });
         }
     };
 });
--- public/js/services/meus-servisos.js
+++ public/js/services/meus-servisos.js
@@ -10,40 +10,40 @@ angular.module('meusServisos', ['ngResource']).factory('recursoFoto', function (
 
 }).factory('cadastroDeFoto', function (recursoFoto, $q) {//$q seria o nosso queue
 
-    var servico = {};
+    var service = {};
 
-    servico.cadastrar = function (foto) {//cadastrar recebe uma foto que irá retornar uma promisse porque retorna uma $q
+    service.cadastrar = function (foto) {//cadastrar recebe uma foto que irá retornar uma promisse porque retorna uma $q
         return $q(function (resolve, reject) {
-            if (foto.id) {//se foto.id for preenchida entra aki
+            if (foto._id) {//se foto.id for preenchida entra aki
 
                 recursoFoto.update({ fotoId: foto._id }, foto, function () {//caso seja um update que eu fiz na foto entra aki
                     resolve({
                         mensagem:'Foto' + foto.titulo + 'Atualizada com Sucesso!!',
-                        inclusão: false //objeto que será enviado caso seja um update
+                        inclusao: false //objeto que será enviado caso seja um update
 
             });
                 }, function (erro) {//caso apresentar um erro no if
                     console.log(erro);
                     reject({
-                        mensagem: 'Não foi possivel alterar a foto' + foto.titulo });
+                        mensagem: 'Infelismente não foi possivel alterar a foto' + foto.titulo });
                 });
 
             } else {//caso a foto seja nova e precisa ser salva
                 recursoFoto.save(foto,function() {
                     resolve({//o resolve tem que passar um objeto para ele
                         mensagem: 'Foto' + foto.titulo + 'Incluída com Sucesso!!',
-                        inclusão: true
+                        inclusao: true
                     }); 
                 }, function(erro) { //caso apresente um erro no else
                     console.log(erro);
                     reject({
-                        mensagem: 'Não foi possivel salvar a foto' + foto.titulo });
+                        mensagem: 'Infelismente não foi possivel salvar a foto' + foto.titulo });
                 });
             }
 
         });
     };
 
-        return servico;
+        return service;
 
 });
